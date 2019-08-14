798381e59da153f1514be777e1006aeaddd409f9
Matt Ellis <matt@pulumi.com>
2019-08-14 15:10:58 -0700

    Initial commit

---
+++ pulumi.rb
@@ -0,0 +1,40 @@
+class Pulumi < Formula
+  desc "Cloud native development platform"
+  homepage "https://pulumi.io/"
+  url "https://github.com/pulumi/pulumi.git",
+      :tag      => "v1.0.0-beta.2",
+      :revision => "fdfef5f334deac2cfe83288a99f22b73995ae736"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
+
+    dir = buildpath/"src/github.com/pulumi/pulumi"
+    dir.install buildpath.children
+
+    cd dir do
+      system "go", "mod", "vendor"
+      system "make", "dist"
+      bin.install Dir["#{buildpath}/bin/*"]
+      prefix.install_metafiles
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/pulumi gen-completion bash")
+      (bash_completion/"pulumi").write output
+
+      # Install zsh completion
+      output = Utils.popen_read("#{bin}/pulumi gen-completion zsh")
+      (zsh_completion/"_pulumi").write output
+    end
+  end
+
+  test do
+    ENV["PULUMI_ACCESS_TOKEN"] = "local://"
+    ENV["PULUMI_TEMPLATE_PATH"] = testpath/"templates"
+    system "#{bin}/pulumi", "new", "aws-typescript", "--generate-only",
+                                                     "--force", "-y"
+    assert_predicate testpath/"Pulumi.yaml", :exist?, "Project was not created"
+  end
+end
