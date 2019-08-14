7202405417a3523790443106285a5879e743a8dd
Matt Ellis <matt@pulumi.com>
2019-08-14 16:21:55 -0700

    WIP

--- pulumi.rb
+++ pulumi.rb
@@ -3,20 +3,19 @@ class Pulumi < Formula
   homepage "https://pulumi.io/"
   version "1.0.0-beta.1"
   url "https://get.pulumi.com/releases/sdk/pulumi-v1.0.0-beta.1-darwin-x64.tar.gz"
-  sha256 ""
+  sha256 "678e0ec39c57b0e251cec728874756241a4cc949ce354915edd70accf250e8d6"
 
   def install
-      bin.install Dir["#{buildpath}/*"]
-      prefix.install_metafiles
+    bin.install Dir["#{buildpath}/*"]
+    prefix.install_metafiles
 
-      # Install bash completion
-      output = Utils.popen_read("#{bin}/pulumi gen-completion bash")
-      (bash_completion/"pulumi").write output
+    # Install bash completion
+    output = Utils.popen_read("#{bin}/pulumi gen-completion bash")
+    (bash_completion/"pulumi").write output
 
-      # Install zsh completion
-      output = Utils.popen_read("#{bin}/pulumi gen-completion zsh")
-      (zsh_completion/"_pulumi").write output
-    end
+    # Install zsh completion
+    output = Utils.popen_read("#{bin}/pulumi gen-completion zsh")
+    (zsh_completion/"_pulumi").write output
   end
 
   test do
